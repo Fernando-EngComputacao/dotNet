bb571bd45598c657d957c42145be1de8bed1ea07
Fernando Souza Furtado Carrilho <fernando.furtado@compasso.com.br>
2022-10-15 19:41:20 -0300

    First Commit

---
+++ Movie/Controllers/FilmeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Movie.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public Filme AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+            return filme;
+        }
+
+        [HttpGet]
+        public List<Filme> RecuperarFilmes()
+        {
+            return filmes;
+        }
+    }
+}
---
+++ Movie/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movie.Models
+{
+    public class Filme
+    {
+        [Required]
+        public string Titulo { get; set; }
+        [Required]
+        public string Diretor { get; set; }
+        [Required]
+        public string Genero { get; set; }
+        [Required]
+        [Range(1,600)]
+        public int Duracao { get; set; }
+
+    }
+}
